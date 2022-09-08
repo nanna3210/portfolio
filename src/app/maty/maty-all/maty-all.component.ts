d78baff0dc33fee310d84ce52f9278a94e07de73
nanna3210 <navkishoredas@gmail.com>
2022-09-08 23:18:55 +0530

    working

--- src/app/maty/maty-all/maty-all.component.ts
+++ src/app/maty/maty-all/maty-all.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {MatChipInputEvent} from '@angular/material/chips';
+// import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-maty-all',
@@ -11,6 +12,7 @@ export class MatyAllComponent implements OnInit {
 
   constructor() { }
   panelOpenState :boolean = false ; 
+
   ngOnInit(): void {
   }
 
