ccc419a0646959b5e61156121121c2c6d29fdbbd
nanna3210 <navkishoredas@gmail.com>
2022-09-23 23:19:48 +0530

    chip

--- src/app/maty/maty-all/maty-all.component.ts
+++ src/app/maty/maty-all/maty-all.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {MatChipInputEvent} from '@angular/material/chips';
+import {ThemePalette} from '@angular/material/core'
 // import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+
+
+
+export interface ChipColor {
+  name: string;
+  color: ThemePalette;
+}
+
 
 @Component({
   selector: 'app-maty-all',
@@ -11,7 +20,13 @@ import {MatChipInputEvent} from '@angular/material/chips';
 export class MatyAllComponent implements OnInit {
 
   constructor() { }
-  panelOpenState :boolean = false ; 
+  panelOpenState :boolean = false ;
+  availableColors: ChipColor[] = [
+    {name: 'none', color: undefined},
+    {name: 'Primary', color: 'primary'},
+    {name: 'Accent', color: 'accent'},
+    {name: 'Warn', color: 'warn'},
+  ]; 
 
   ngOnInit(): void {
   }
